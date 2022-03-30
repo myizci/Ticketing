a7a04fe691f9ae18464be06c25c20cd2e99d91e4
myizci <izci@hotmail.com>
2022-03-29 21:38:50 -0400

    Day3 Project Create is halfway

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,8 +1,11 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import com.cydeo.service.impl.RoleServiceImpl;
@@ -10,27 +13,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
-public class DataGenerator  implements CommandLineRunner { // this class will run first bc of CommandLineRunner
+public class DataGenerator implements CommandLineRunner { // this class will run first bc of CommandLineRunner
 
 
-RoleService roleService; // interface reference
-UserService userService; // interface
+    RoleService roleService;
+    UserService userService;
+    ProjectService projectService;
 
-    public DataGenerator(RoleService roleService, UserService userService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
         this.roleService = roleService;
         this.userService = userService;
-    } // injecting data into role object
+        this.projectService = projectService;
+    }
 
-
-
-        @Override
+    @Override
     public void run(String... args) throws Exception {
 
-        RoleDTO adminRole = new RoleDTO(1L,"Admin");
-        RoleDTO managerRole = new RoleDTO(2L,"Manager");
-        RoleDTO employeeRole = new RoleDTO(3L,"Employee");
-
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
 
 
         roleService.save(adminRole);
@@ -65,7 +69,14 @@ UserService userService; // interface
         userService.save(user8);
 
 
+        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user1, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.IN_PROGRESS);
+
+        projectService.save(project1);
+        projectService.save(project2);
+        projectService.save(project3);
 
 
-        }
+    }
 }
---
+++ src/main/java/com/cydeo/enums/Status.java
@@ -0,0 +1,16 @@
+package com.cydeo.enums;
+
+public enum Status {
+
+    OPEN("Open"), IN_PROGRESS("In_Progress"),COMPLETE("Completed");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
